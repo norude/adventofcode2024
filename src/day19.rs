fb8a4e549a7593928f8125a2a8d61b76ea9af7c6
norude <fedor.tryfanau@gmail.com>
2024-12-19 22:10:49 +0300

    day 19

--- src/day19.rs
+++ src/day19.rs
@@ -3,6 +3,8 @@ use std::{
     io::{self, BufRead},
 };
 
+use cached::proc_macro::cached;
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
 where
     P: AsRef<std::path::Path>,
@@ -10,20 +12,76 @@ where
     let file = fs::File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
-fn read() {
-    let lines = read_lines("day19.input").unwrap();
-    lines
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum Color {
+    White,
+    Blue,
+    Black,
+    Red,
+    Green,
+}
+impl Color {
+    fn from_char(s: char) -> Self {
+        match s {
+            'w' => Self::White,
+            'u' => Self::Blue,
+            'r' => Self::Red,
+            'g' => Self::Green,
+            'b' => Self::Black,
+            _ => unreachable!(),
+        }
+    }
+}
+fn read() -> (Vec<Vec<Color>>, Vec<Vec<Color>>) {
+    let mut lines = read_lines("day19.input")
+        .unwrap()
         .map(Result::unwrap)
-        .filter(|line| !line.is_empty())
-        .collect()
+        .filter(|line| !line.is_empty());
+    (
+        lines
+            .next()
+            .unwrap()
+            .split(", ")
+            .map(|p| p.chars().map(Color::from_char).collect())
+            .collect(),
+        lines
+            .map(|p| p.chars().map(Color::from_char).collect())
+            .collect(),
+    )
+}
+
+#[cached(key = "(usize,usize)", convert = "{(idx,didx)}")]
+fn fits(idx: usize, didx: usize, designs: &[Vec<Color>], towels: &[Vec<Color>]) -> usize {
+    let design = &designs[didx];
+    if idx == design.len() {
+        return 1;
+    }
+    (idx..design.len())
+        .map(|i| {
+            if towels.iter().any(|p| p == &design[idx..=i]) {
+                fits(i + 1, didx, designs, towels)
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 pub fn a() {
-    let s = read();
+    let (towels, designs) = read();
+    let s = designs
+        .iter()
+        .enumerate()
+        .filter(|design| fits(0, design.0, &designs, &towels) > 0)
+        .count();
     println!("{s}");
 }
 pub fn b() {
-    let s = read();
+    let (towels, designs) = read();
+    let s: usize = designs
+        .iter()
+        .enumerate()
+        .map(|design| fits(0, design.0, &designs, &towels))
+        .sum();
     println!("{s}");
 }
