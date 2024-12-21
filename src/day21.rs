652487b3eea5df4ee5e3c6602ae099b2d688e43e
norude <fedor.tryfanau@gmail.com>
2024-12-21 23:33:41 +0300

    day21

--- src/day21.rs
+++ src/day21.rs
@@ -3,6 +3,8 @@ use std::{
     io::{self, BufRead},
 };
 
+use cached::proc_macro::cached;
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
 where
     P: AsRef<std::path::Path>,
@@ -11,7 +13,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn read() {
+fn read() -> Vec<String> {
     let lines = read_lines("day21.input").unwrap();
     lines
         .map(Result::unwrap)
@@ -19,11 +21,110 @@ fn read() {
         .collect()
 }
 
-pub fn a() {
-    let s = read();
-    println!("{s}");
+fn pad0(num: &str) -> String {
+    let mut res = String::new();
+    let (mut x, mut y) = (2, 3);
+    for n in num.chars() {
+        let (i, j) = match n {
+            '7' => (0, 0),
+            '8' => (1, 0),
+            '9' => (2, 0),
+            '4' => (0, 1),
+            '5' => (1, 1),
+            '6' => (2, 1),
+            '1' => (0, 2),
+            '2' => (1, 2),
+            '3' => (2, 2),
+            '0' => (1, 3),
+            'A' => (2, 3),
+            _ => unreachable!(),
+        };
+
+        if x == 0 && j == 3 {
+            res += &">".repeat(i - x);
+            res += &"v".repeat(j - y);
+        } else if y == 3 && i == 0 {
+            res += &"^".repeat(y - j);
+            res += &"<".repeat(x - i);
+        } else {
+            if x > i {
+                res += &"<".repeat(x - i);
+            }
+            if j > y {
+                res += &"v".repeat(j - y);
+            }
+            if y > j {
+                res += &"^".repeat(y - j);
+            }
+            if i > x {
+                res += &">".repeat(i - x);
+            }
+        }
+
+        res.push('A');
+        (x, y) = (i, j);
+    }
+    res
 }
+
+fn pad1(num: &str) -> Vec<String> {
+    let mut res: Vec<String> = vec![];
+    let mut prev = 'A';
+    for n in num.chars() {
+        res.push(
+            match (prev, n) {
+                ('^', '^') | ('v', 'v') | ('A', 'A') | ('<', '<') | ('>', '>') => "A",
+                ('A', '^') | ('v', '<') | ('>', 'v') => "<A",
+                ('^', 'A') | ('<', 'v') | ('v', '>') => ">A",
+                ('v', '^') | ('>', 'A') => "^A",
+                ('^', 'v') | ('A', '>') => "vA",
+                ('^', '>') => "v>A",
+                ('>', '^') => "<^A",
+                ('A', 'v') => "<vA",
+                ('v', 'A') => "^>A",
+
+                ('<', '^') => ">^A",
+                ('^', '<') => "v<A",
+                ('<', '>') => ">>A",
+                ('>', '<') => "<<A",
+                ('<', 'A') => ">>^A",
+                ('A', '<') => "v<<A",
+                _ => unreachable!(),
+            }
+            .to_string(),
+        );
+        prev = n;
+    }
+    res
+}
+
+#[cached]
+fn pad1_repeat(num: String, i: u32) -> u128 {
+    if i == 0 {
+        return num.len() as u128;
+    }
+    pad1(&num)
+        .into_iter()
+        .map(|num| pad1_repeat(num, i - 1))
+        .sum()
+}
+
+pub fn a() {
+    let res = read()
+        .iter()
+        .map(|num| {
+            pad1_repeat(pad0(num), 2) * num.strip_suffix(&"A").unwrap().parse::<u128>().unwrap()
+        })
+        .sum::<u128>();
+    println!("{res}");
+}
+
 pub fn b() {
-    let s = read();
-    println!("{s}");
+    let res = read()
+        .iter()
+        .map(|num| {
+            pad1_repeat(pad0(num), 25) * num.strip_suffix(&"A").unwrap().parse::<u128>().unwrap()
+        })
+        .sum::<u128>();
+    println!("{res}");
 }
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ mod day17;
 mod day18;
 mod day19;
 mod day20;
-// mod day21;
+mod day21;
 // mod day22;
 // mod day23;
 // mod day24;
@@ -85,7 +85,7 @@ fn main() {
         SelectedDay::Day18 => { print!("day 18 part a:"); day18::a(); print!("day 18 part b:"); day18::b(); }
         SelectedDay::Day19 => { print!("day 19 part a:"); day19::a(); print!("day 19 part b:"); day19::b(); }
         SelectedDay::Day20 => { print!("day 20 part a:"); day20::a(); print!("day 20 part b:"); day20::b(); }
-        SelectedDay::Day21 => { /* print!("day 21 part a:"); day21::a(); print!("day 21 part b:"); day21::b(); */ }
+        SelectedDay::Day21 => { print!("day 21 part a:"); day21::a(); print!("day 21 part b:"); day21::b(); }
         SelectedDay::Day22 => { /* print!("day 22 part a:"); day22::a(); print!("day 22 part b:"); day22::b(); */ }
         SelectedDay::Day23 => { /* print!("day 23 part a:"); day23::a(); print!("day 23 part b:"); day23::b(); */ }
         SelectedDay::Day24 => { /* print!("day 24 part a:"); day24::a(); print!("day 24 part b:"); day24::b(); */ }
